e123dbe4876d3465ea5ae2e4735c7c9f8e654b3a
NeuralNat <ifrance@yandex.ru>
2023-11-25 12:26:20 +0300

    @media footer, JS hide mobile-nav on window click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 const navBtn = document.querySelector('.nav-button');
 const navMobile = document.querySelector('.mobile-nav');
+const body = document.body;
 
-
-navBtn.addEventListener('click', function(){
+// Клик по кнопке
+navBtn.addEventListener('click', function (event) {
+    event.stopImmediatePropagation();
     navMobile.classList.toggle('mobile-nav-active');
     navBtn.classList.toggle('nav-button-close');
+    body.classList.toggle('no-scroll');
+});
+
+//Клик по окну , за пределами навигации 
+window.addEventListener('click', function (){
+    console.log('gggggg');
+    if(body.classList.contains('no-scroll')) {
+        body.classList.toggle('no-scroll');
+        navBtn.classList.toggle('nav-button-close');
+        navMobile.classList.toggle('mobile-nav-active');
+    };
+});
+
+// Останавливаем клик внутри открытой мобильной навигации
+navMobile.addEventListener('click', function(event){
+    event.stopPropagation();
 });
