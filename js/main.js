a901b603b6369419fca79c6b684afbe44b65c437
NeuralNat <ifrance@yandex.ru>
2023-11-25 12:37:53 +0300

    JS function toggleMobileNav();

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
-const navBtn = document.querySelector('.nav-button');
-const navMobile = document.querySelector('.mobile-nav');
+const navBtn = document.querySelector(".nav-button");
+const navMobile = document.querySelector(".mobile-nav");
 const body = document.body;
 
 // Клик по кнопке
-navBtn.addEventListener('click', function (event) {
-    event.stopImmediatePropagation();
-    navMobile.classList.toggle('mobile-nav-active');
-    navBtn.classList.toggle('nav-button-close');
-    body.classList.toggle('no-scroll');
+navBtn.addEventListener("click", function (event) {
+  event.stopImmediatePropagation();
+  toggleMobileNav();
 });
 
-//Клик по окну , за пределами навигации 
-window.addEventListener('click', function (){
-    console.log('gggggg');
-    if(body.classList.contains('no-scroll')) {
-        body.classList.toggle('no-scroll');
-        navBtn.classList.toggle('nav-button-close');
-        navMobile.classList.toggle('mobile-nav-active');
-    };
+//Клик по окну , за пределами навигации
+window.addEventListener("click", function () {
+  if (body.classList.contains("no-scroll")) {
+    toggleMobileNav();
+  }
 });
 
 // Останавливаем клик внутри открытой мобильной навигации
-navMobile.addEventListener('click', function(event){
-    event.stopPropagation();
+navMobile.addEventListener("click", function (event) {
+  event.stopPropagation();
 });
+
+function toggleMobileNav() {
+  body.classList.toggle("no-scroll");
+  navBtn.classList.toggle("nav-button-close");
+  navMobile.classList.toggle("mobile-nav-active");
+}
